bfa65af536357da00792525f1db1c8cc99970dc2
João Porto <joaofporto@tecnico.ulisboa.pt>
2020-03-15 16:23:20 +0000

    Refactored some code

--- solver.cpp
+++ solver.cpp
@@ -4,8 +4,6 @@
 #include <string>
 #include <stack>
 
-#define MIN(a, b) (a < b ? a : b)
-
 using namespace std;
 
 /*
@@ -28,14 +26,12 @@ grade (of student 4)
 */
 
 // ###################### Global variables ####################################################
-int studentNum;                 // Number of vertices
-int linkNum;                    // Number of edges
-vector<vector<uint> *> friends; // Vector of vertices
-vector<uint> students;          // List of students
-int *grades;                    // List of grades
-bool *visited;                  // List with visited status of students
-
-// ###################### Node ################################################################
+int studentNum;               // Number of vertices
+int linkNum;                  // Number of edges
+vector<vector<uint>> friends; // Vector of vertices
+uint *students;               // List of students
+ushort *grades;               // List of grades
+bool *visited;                // List with visited status of students
 
 // ###################### Input Handling ######################################################
 void readInput()
@@ -44,97 +40,91 @@ void readInput()
     if (!scanf("%u,%u", &studentNum, &linkNum))
         exit(-1);
 
-    // Shorten the nodes list
-    friends.resize(studentNum);
-
     // Initialize all the arrays
-    grades = new int[studentNum];
+    grades = new ushort[studentNum];
     visited = new bool[studentNum];
-    students = vector<uint>();
-    students.resize(studentNum);
+    students = new uint[studentNum];
+    friends = vector<vector<uint>>(studentNum);
 
-    // Initialize the nodes
+    // Read the student grades
     ushort grade = 0;
     for (int i = 0; i < studentNum; i++)
     {
         if (!scanf("%hu", &grade))
             exit(-1);
         else
-            friends[i] = new vector<uint>();
+            friends[i] = vector<uint>();
         grades[i] = grade;
         students[i] = i;
     }
 
-    // Update the adjacencies list of each vertex
+    // Read friendship list
     unsigned int id = 0;
     unsigned int friendId = 0;
     for (int i = 0; i < linkNum; i++)
         if (scanf("%u %u", &id, &friendId) > 0)
-            (*friends[friendId - 1]).push_back(id - 1);
+            friends[friendId - 1].push_back(id - 1);
         else
             exit(-1);
 }
 
-// ###################### Algorithms  #########################################################
+// ###################### Depth-first search ##################################################
 void propagate(uint start)
 {
 
     stack<uint> stack;
-    uint s;
+    uint friendId;
     // Push the current source node.
     stack.push(start);
     int bestGrade = grades[start];
 
     while (!stack.empty())
     {
-        // Pop a vertex from stack and print it
-        s = stack.top();
+        friendId = stack.top();
         stack.pop();
 
-        // Stack may contain same vertex twice. So
-        // we need to print the popped item only
-        // if it is not visited.
-        if (!visited[s])
+        // if it is not visited, recalculate grade
+        if (!visited[friendId])
         {
-            int currentGrade = grades[s];
+            int currentGrade = grades[friendId];
             if (currentGrade > bestGrade)
                 continue;
             else
-                grades[s] = bestGrade;
+                grades[friendId] = bestGrade;
 
-            visited[s] = true;
+            visited[friendId] = true;
         }
 
-        // Get all adjacent vertices of the popped vertex s
+        // Get all buddies of the students s
         // If a adjacent has not been visited, then push it
         // to the stack.
-        for (uint buddy : *friends[s])
+        for (uint buddy : friends[friendId])
             if (!visited[buddy])
                 stack.push(buddy);
     }
 }
 
-// For the sorting algorithm
-bool compare(const int a, const int b)
-{
-    return grades[a] < grades[b];
-}
+// Sort student id by grades
+bool compare(const int a, const int b) { return grades[a] < grades[b]; }
 
 // ###################### Main ################################################################
 int main()
 {
-    friends = vector<vector<uint> *>();
+
+    friends = vector<vector<uint>>();
 
     // Input
     readInput();
 
-    sort(students.begin(), students.end(), compare);
-    while (!students.empty())
+    // Sort student grades
+    sort(students, students + studentNum, compare);
+    
+    // Recalculate grades
+    for (int i = studentNum - 1; i >= 0; i--)
     {
-        uint student = students.back();
+        uint student = students[i];
         if (!visited[student])
             propagate(student);
-        students.pop_back();
     }
 
     // Print result
@@ -144,6 +134,8 @@ int main()
     // Free allocs
     delete[] grades;
     delete[] visited;
+    delete[] students;
+    friends.clear();
 
     return 0;
 }
